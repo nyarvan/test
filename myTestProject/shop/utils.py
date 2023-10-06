8c4311ef0b8b3292800244b7bf88cef8af97231a
Ivan <infinity.man102@gmail.com>
2023-10-06 16:53:32 +0300

    Add return count of element in cart

--- myTestProject/shop/utils.py
+++ myTestProject/shop/utils.py
@@ -3,3 +3,9 @@ def showMessage(message):
 
 def avgPriceProducts(product_prices):
     return sum(product_prices)/len(product_prices)
+
+def getNameOfBrand(product):
+    return product.brand
+
+def getCountOfCart(cart):
+    return len(cart)
